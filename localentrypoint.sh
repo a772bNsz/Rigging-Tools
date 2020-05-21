73fcaf5bb93cd877c6f238c0226489e081a2b428
a772bNsz <kathyhuinali@gmail.com>
2020-05-21 16:45:56 -0400

    updating entrypoint scripts

--- localentrypoint.sh
+++ localentrypoint.sh
@@ -3,5 +3,5 @@ set -e
 echo ">>>>> MAYAPY"
 mayapy -m tests.test_sample
 COPY_FROM=/root/maya/projects/default/scenes/result.ma
-COPY_TO=tests/ma/result.ma
+COPY_TO=results/result.ma
 cp $COPY_FROM $COPY_TO 2>/dev/null || :
