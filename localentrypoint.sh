4f9aeedbc17a6a122ac987ffb4a32249e88dc753
a772bNsz <kathyhuinali@gmail.com>
2020-05-21 18:13:14 -0400

    tests return results to /results

--- localentrypoint.sh
+++ localentrypoint.sh
@@ -2,6 +2,6 @@
 set -e
 echo ">>>>> MAYAPY"
 mayapy -m tests.test_color_shapes
-COPY_FROM=/root/maya/projects/default/scenes/result.ma
-COPY_TO=results/result.ma
+COPY_FROM=/root/maya/projects/default/scenes/test_color_shapes.ma
+COPY_TO=results/test_color_shapes.ma
 cp $COPY_FROM $COPY_TO 2>/dev/null || :
