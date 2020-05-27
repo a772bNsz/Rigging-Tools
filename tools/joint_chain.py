494fe246091e8bbe1b1f727845e5c82c8fbe1be9
a772bNsz <kathyhuinali@gmail.com>
2020-05-27 12:17:16 -0400

    joint_chain()

---
+++ tools/joint_chain.py
@@ -0,0 +1,12 @@
+import pymel.core as pm
+
+
+def joint_chain(curve, number=2, name="joint#", root=None):
+    pm.select(cl=1)
+    for i in range(number):
+        percent = i / (number - 1.0)
+        position = pm.pointOnCurve(curve, pr=percent, p=1, top=1)
+        joint = pm.joint(p=position, a=1, n=name.replace("#", str(i+1)))
+        if i == 0:
+            root = joint
+    return root
