e03b20a919ea31e6a555a6b4b9ebf3f14bf68dee
a772bNsz <kathyhuinali@gmail.com>
2020-06-24 17:57:33 -0400

    test arm

--- localentrypoint.sh
+++ localentrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo ">>>>> MAYAPY"
-mayapy -m tests.test_leg
+mayapy -m tests.test_arm
 COPY_FROM=/root/maya/projects/default/scenes/result.ma
 COPY_TO=results/result.ma
 cp $COPY_FROM $COPY_TO 2>/dev/null || :
