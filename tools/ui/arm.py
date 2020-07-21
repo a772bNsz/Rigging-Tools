57b772b80863a13dd9f26125f036694ce91cc167
a772bNsz <kathyhuinali@gmail.com>
2020-07-21 19:29:46 -0400

    UI version 2

--- tools/ui/arm.py
+++ tools/ui/arm.py
@@ -7,9 +7,8 @@ from maya.OpenMayaUI import MQtUtil
 from pymel.util.path import path
 
 import pymel.core as pm
-from tools.arm import Rig as Arm
-from tools.shoulder import Rig as Shoulder
-from tools.hand import Rig as Hand
+from tools import color_shapes
+reload(color_shapes)
 
 
 def get_window():
@@ -44,16 +43,7 @@ class MyWindow(QWidget):
         self.setWindowTitle(window_title)
         self.init_ui()
 
-        self.shoulder = None
-        self.left_shoulder = None
-        self.right_shoulder = None
-        self.arm = None
-        self.left_arm = None
-        self.right_arm = None
-        self.locators = {}
-        self.hand = None
-        self.left_hand = None
-        self.right_hand = None
+        self.color = color_shapes.ColorShapes()
 
     def import_ui(self):
         """
@@ -77,245 +67,234 @@ class MyWindow(QWidget):
         self.ui.add_btn.clicked.connect(self._add_list_widget_item)
         self.ui.minus_btn.clicked.connect(self._minus_list_widget_item)
 
-        self.ui.shoulder_right_btn.clicked.connect(
-            lambda x="right": self.rig_shoulder(x))
-        self.ui.shoulder_mid_btn.clicked.connect(
-            lambda x="": self.rig_shoulder(x))
-        self.ui.shoulder_left_btn.clicked.connect(
-            lambda x="left": self.rig_shoulder(x))
+        self.ui.guides_btn.clicked.connect(self.create_guides)
+        self.ui.match_left_to_right_btn.clicked.connect(self.match)
 
-        self.ui.arm_right_btn.clicked.connect(
-            lambda x="right": self.rig_arm(x))
-        self.ui.arm_mid_btn.clicked.connect(
-            lambda x="": self.rig_arm(x))
-        self.ui.arm_left_btn.clicked.connect(
-            lambda x="left": self.rig_arm(x))
+        self.ui.rig_left_btn.clicked.connect(lambda x="left": self.rig(x))
+        self.ui.rig_right_btn.clicked.connect(lambda x="right": self.rig(x))
 
-        self.ui.setup_right_btn.clicked.connect(
-            lambda x="right": self.setup_hand(x))
-        self.ui.setup_mid_btn.clicked.connect(
-            lambda x="": self.setup_hand(x))
-        self.ui.setup_left_btn.clicked.connect(
-            lambda x="left": self.setup_hand(x))
-
-        self.ui.hand_right_btn.clicked.connect(
-            lambda x="right": self.rig_hand(x))
-        self.ui.hand_mid_btn.clicked.connect(
-            lambda x="": self.rig_hand(x))
-        self.ui.hand_left_btn.clicked.connect(
-            lambda x="left": self.rig_hand(x))
+        self.ui.minus_90_btn.clicked.connect(lambda x="minus": self.rotate(x))
+        self.ui.add_90_btn.clicked.connect(lambda x="add": self.rotate(x))
         return
 
-    def rig_hand(self, side):
-        side = side if "" == side else side + "_"
-        hand = getattr(self, side + "hand")
+    def rig(self, side):
+        shoulder, arm, hand = \
+            ["_".join([side, n]) for n in ["shoulder", "arm", "hand"]]
+        
+        if pm.objExists(shoulder):
+            self.rebuild_shoulder(side)
+            # self.rig_shoulder()
 
-        # setup_hand() cleanup
-        thumb = hand.result_chain["thumb"]
+        if pm.objExists(arm):
+            self.rebuild_arm(side)
+            # self.rig_arm()
 
-        thumb_start = thumb["base"]
-        aim_setup = thumb_start.inputs(type="aimConstraint")[0]
-        aim_setup = aim_setup.target.inputs()
-        pm.delete(aim_setup)
-        pm.makeIdentity(thumb_start, apply=1, r=1)
+        if pm.objExists(hand):
+            self.rebuild_hand(side)
+            # self.rig_hand()
 
-        palm_locators_group = self.locators[0].getParent()
-        pm.parent(self.locators, w=1)
-        pm.delete(palm_locators_group)
-
-        # rig_hand()
-        names = ["index", "middle", "ring", "pinky"]
-        hand.finger_attributes(fingers=names)
-
-        params = {
-            "palm": self.locators,
-            "fingers": names
-        }
-        hand.palm_attributes(**params)
-
-        params = {}
-        try:
-            arm = getattr(self, side + "arm")
-            params = {
-                "control": arm.result_chain["hand"],
-                "bind_joint": arm.twist_nodes["lower"]["end_bind"],
-                "settings": arm.controls["arm_settings"],
-            }
-        except:
-            pass
-        hand.connect(**params)
+        #TODO: delete locators
         return
 
-    def setup_hand(self, side):
-        hand_joint = pm.ls(sl=1)[0]
+    @staticmethod
+    def rebuild_hand(side):
+        guide_start = pm.PyNode("_".join([side, "hand"]))
+        finger_guide_roots = guide_start.getChildren()
+
+        for root in finger_guide_roots:
+            pm.select(cl=1)
+
+            chain = []
+            guides = [root] + root.getChildren(ad=1)[::-1]
+            for guide in guides:
+                pos = guide.getTranslation(ws=1)
+                chain += [pm.joint(p=pos)]
+
+            pm.joint(chain, e=1, oj="xyz", sao="ydown", ch=1, zso=1)
+            chain[-1].jointOrient.set(0, 0, 0)
+        return
+
+    @staticmethod
+    def rebuild_arm(side):
+        guide_start = pm.PyNode("_".join([side, "arm"]))
+        guide_end = pm.PyNode("_".join([side, "hand"]))
+
+        guides = [guide_start] + guide_start.getChildren(ad=1)[::-1]
+        i = guides.index(guide_end) + 2
+        guides = guides[:i]  # arm to hand end
+
+        chain = []
         pm.select(cl=1)
+        for guide in guides:
+            pos = guide.getTranslation(ws=1)
+            chain += [pm.joint(p=pos)]
+        chain[-1].ty.set(0)
+        chain[-1].tz.set(0)
 
-        root = pm.joint(p=[68.17, 138.49, 2.02])
+        pm.joint(chain, e=1, oj="xyz", sao="ydown", ch=1, zso=1)
+        return chain
+
+    @staticmethod
+    def rebuild_shoulder(side):
+        guide_start = pm.PyNode("_".join([side, "shoulder"]))
+        guide_end = guide_start.getChildren()[0]
+
+        chain = []
+        pm.select(cl=1)
+        for guide in [guide_start, guide_end]:
+            pos = guide.getTranslation(ws=1)
+            chain += [pm.joint(p=pos)]
+
+        pm.joint(chain, e=1, oj="xyz", sao="yup", ch=1, zso=1)
+        return chain
+
+    def match(self):
+        # locators
+        left_locators = \
+            [loc.getParent() for loc in pm.ls("left_*", type="locator")]
+        right_locators = \
+            [loc.getParent() for loc in pm.ls("right_*", type="locator")]
+
+        for right, left in zip(right_locators, left_locators):
+            match = right.replace("right", "left") == left
+            if not match:
+                right = pm.spaceLocator(n=left.replace("left", "right"))
+
+                self.color.sel = right
+                self.color.index = 12  # red right
+                self.color.by_index()
+
+            pos = left.getTranslation(space="world")
+            pos[0] = pos[0] * -1
+            right.setTranslation(pos, space="world")
+
+            size = left.localScale.get()
+            right.localScale.set(size)
+
+            if "aim" not in str(left):
+                left.rotate.set(0, 0, 0)
+                right.rotate.set(0, 0, 0)
+
+        # joints
+        left_chain, = filter(
+            lambda x: pm.nodeType(x) == "joint", pm.ls("left*", assemblies=1))
+        left_chain = [left_chain] + \
+                     left_chain.getChildren(ad=1, type="joint")[::-1]
+
+        right_chain, = filter(
+            lambda x: pm.nodeType(x) == "joint", pm.ls("right*", assemblies=1))
+        right_chain = [right_chain] + \
+                      right_chain.getChildren(ad=1, type="joint")[::-1]
+
+        for right, left in zip(right_chain, left_chain):
+            pos = left.getTranslation(space="world")
+            pos[0] = pos[0] * -1
+            right.setTranslation(pos, space="world")
+
+            size = left.radius.get()
+            right.radius.set(size)
+        return
+
+    @staticmethod
+    def orbit_guide(side, thumb):
+        aim_loc = pm.spaceLocator(n="_".join([side, "aim"]))
+        up_loc = pm.spaceLocator(n="_".join([side, "up"]))
+        up_loc.setParent(aim_loc)
+
+        thumb_end = thumb.getChildren(ad=1)[0]
+        pm.matchTransform(aim_loc, thumb_end)
+
+        aim_const = pm.aimConstraint(aim_loc, thumb,
+                                     mo=1,
+                                     aimVector=[1, 0, 0],
+                                     upVector=[0, 1, 0],
+                                     worldUpType="objectRotation",
+                                     worldUpVector=[0, 1, 0],
+                                     worldUpObject=up_loc)
+        return [aim_loc, up_loc], aim_const
+
+    def create_guides(self):
+        # left
+        arm = pm.joint(p=[8.91, 142.2, 0.97], n="left_shoulder")  # shoulder
+        pm.joint(p=[17.19, 138.49, 0.94], n="left_arm")  # upper arm
+        pm.joint(p=[40.69, 138.49, 1.44])  # lower arm
+        root = pm.joint(p=[68.17, 138.49, 2.02], n="left_hand")  # hand
+        # pm.joint(p=[75.66, 138.49, 2.18])
 
         pm.select(root)
-        pinky = pm.joint(p=[72.26, 138.44, -0.63], n="pinky")
+        pinky = pm.joint(p=[72.26, 138.44, -0.63], n="left_pinky")
         pm.joint(p=[76.93, 138.44, -0.9])
         pm.joint(p=[79.19, 138.44, -0.9])
         pm.joint(p=[81.26, 138.44, -0.9])
         pm.joint(p=[83.04, 138.44, -0.9])
 
         pm.select(root)
-        ring = pm.joint(p=[72.32, 138.85, 0.79], n="ring")
+        ring = pm.joint(p=[72.32, 138.85, 0.79], n="left_ring")
         pm.joint(p=[77.23, 138.85, 0.67])
         pm.joint(p=[80.56, 138.85, 0.67])
         pm.joint(p=[83.09, 138.85, 0.67])
         pm.joint(p=[85.03, 138.85, 0.67])
 
         pm.select(root)
-        middle = pm.joint(p=[72.32, 139.11, 2.14], n="middle")
+        middle = pm.joint(p=[72.32, 139.11, 2.14], n="left_middle")
         pm.joint(p=[77.59, 139.11, 2.3])
         pm.joint(p=[81.01, 139.11, 2.3])
         pm.joint(p=[83.58, 139.11, 2.3])
         pm.joint(p=[85.66, 139.11, 2.3])
 
         pm.select(root)
-        index = pm.joint(p=[72.23, 139.11, 3.62], n="index")
+        index = pm.joint(p=[72.23, 139.11, 3.62], n="left_index")
         pm.joint(p=[77.74, 139.11, 3.87])
         pm.joint(p=[80.85, 139.11, 3.87])
         pm.joint(p=[83.39, 139.11, 3.87])
         pm.joint(p=[85.33, 139.11, 3.87])
 
         pm.select(root)
-        thumb = pm.joint(p=[71.11, 137.67, 5.15], n="thumb")
+        thumb = pm.joint(p=[71.11, 137.67, 5.15], n="left_thumb")
         pm.joint(p=[73.27, 136.91, 6.71])
         pm.joint(p=[74.76, 136.39, 7.78])
         pm.joint(p=[76.83, 135.66, 9.28])
 
-        fingers = {
-            "thumb": thumb,
-            "index": index,
-            "middle": middle,
-            "ring": ring,
-            "pinky": pinky
-        }
+        chain = [arm] + arm.getChildren(ad=1)[::-1]
+        pm.joint(chain, e=1, oj="xyz", sao="ydown", ch=1, zso=1)
+        for finger_root in [thumb, index, middle, ring, pinky]:
+            tip_joint = finger_root.getChildren(ad=1)[0]
+            tip_joint.jointOrient.set(0, 0, 0)
 
-        # palm locators
-        middle = pm.spaceLocator(n="middle")
-        middle.t.set([77.59, 139.11, 2.3])
+        aim_locators, aim_const = self.orbit_guide("left", thumb)
 
-        outer = pm.spaceLocator(n="outer")
-        outer.t.set([75.37, 137.33, -1.16])
-
-        inner = pm.spaceLocator(n="inner")
-        inner.t.set([75.79, 137.55, 4.45])
-
-        self.locators = [middle, inner, outer]
-
-        grp = pm.group(self.locators)
-        grp.setParent(root)
-        pm.matchTransform(root, hand_joint, pos=1)
-
-        if "right" == side:
-            pass
-
-        controls = []
-        i = self.ui.space_switch_lsw.count()
-        for e in range(i):
-            controls += [pm.PyNode(self.ui.space_switch_lsw.item(e).text())]
-
-        root_con = controls[-1]
-        hand = Hand(root, side=side, root_control=root_con)
-        side = side if "" == side else side + "_"
-        setattr(self, side + "hand", hand)
-
-        for k, v in fingers.items():
-            hand.finger_chain(v, name=k)
-
-        aim_loc = pm.spaceLocator(n="aim_LOC")
-        up_loc = pm.spaceLocator(n="up_LOC")
-        up_loc.ty.set(3)
-        up_loc.setParent(aim_loc)
-
-        thumb_finger = hand.result_chain["thumb"].values()
-        thumb_end = thumb_finger[-1]
-        pm.matchTransform(aim_loc, thumb_end)
-
-        thumb_start = thumb_finger[0]
-        pm.aimConstraint(aim_loc, thumb_start,
-                         mo=1,
-                         aimVector=[1, 0, 0],
-                         upVector=[0, 1, 0],
-                         worldUpType="objectRotation",
-                         worldUpVector=[0, 1, 0],
-                         worldUpObject=up_loc)
-        pm.select(aim_loc)
-        return
-
-    def rig_shoulder(self, side):
-        if not pm.ls(sl=1):
-            pm.warning("No root joint selected.")
-            return
-
-        if not self.ui.space_switch_lsw.count():
-            pm.warning("No spaces to connect arm rig.")
-
-        controls = []
-        i = self.ui.space_switch_lsw.count()
-        for e in range(i):
-            controls += [pm.PyNode(self.ui.space_switch_lsw.item(e).text())]
-
-        root = pm.ls(sl=1)[0]
-        root_con = controls[-1]
-        shoulder = Shoulder(root, side=side, root_control=root_con)
-        shoulder.ik()
-        shoulder.connect(control="chest_CON")
-
-        add = True
-        i = self.ui.space_switch_lsw.count()
-        for e in range(i):
-            item = self.ui.space_switch_lsw.item(e).text()
-            if "shoulder" in item.lower():
-                add = False
-                break
-
-        if add:
-            pm.select(shoulder.groups["attach"])
-            self._add_list_widget_item()
-            pm.select(cl=1)
-
-        if "right" == side:
-            self.right_shoulder = shoulder
-        elif "left" == side:
-            self.left_shoulder = shoulder
-        else:
-            self.shoulder = shoulder
-
+        locators = [pm.spaceLocator(n="left_elbow")]
+        pm.matchTransform(locators[-1], chain[2], pos=1)
         pm.select(cl=1)
-        return
 
-    def rig_arm(self, side):
-        if not pm.ls(sl=1):
-            pm.warning("No root joint selected.")
-            return
+        locators += [pm.spaceLocator(n="left_middle")]
+        locators[-1].t.set(77.59, 139.11, 2.3)
 
-        if not self.ui.space_switch_lsw.count():
-            pm.warning("No spaces to connect arm rig.")
+        locators += [pm.spaceLocator(n="left_outer")]
+        locators[-1].t.set(75.37, 137.33, -1.16)
 
-        controls = []
-        i = self.ui.space_switch_lsw.count()
-        for e in range(i):
-            controls += [pm.PyNode(self.ui.space_switch_lsw.item(e).text())]
+        locators += [pm.spaceLocator(n="left_inner")]
+        locators[-1].t.set(75.79, 137.55, 4.45)
 
-        root = pm.ls(sl=1)[0]
-        root_con = controls[-1]
-        arm = Arm(root, side=side, root_control=root_con)
-        arm.twist()
-        arm.ikfk_switch()
-        arm.fk()
-        arm.ik()
-        arm.connect(controls)
+        self.color.sel = locators + aim_locators
+        self.color.index = 6  # blue
+        self.color.by_index()
 
-        if "right" == side:
-            self.right_arm = arm
-        elif "left" == side:
-            self.left_arm = arm
-        else:
-            self.arm = arm
+        # right
+        for i, loc in enumerate(locators):
+            name = loc.replace("left", "right")
+            loc = locators[i] = pm.duplicate(loc, n=name)[0]
+            loc.tx.set(loc.tx.get() * -1)
+
+        pm.PyNode(pm.mirrorJoint(arm, mirrorYZ=1, mirrorBehavior=1,
+                                 searchReplace=["left", "right"])[0])
+
+        thumb = pm.PyNode("right_thumb")
+        aim_locators, aim_const = self.orbit_guide("right", thumb)
+
+        self.color.sel = locators + aim_locators
+        self.color.index = 12  # red right
+        self.color.by_index()
 
         pm.select(cl=1)
         return
@@ -339,15 +318,26 @@ class MyWindow(QWidget):
 
             found = []
             for c in ["CON", "CTL", "CTRL"]:
-                space = item.text()
-                found += pm.ls(space + "*" + c)
-
-                if "shoulder" == space:
-                    found += pm.ls(space.upper() + "_GRP")
+                found += pm.ls(item.text() + "*" + c)
 
             if not found:
                 self.ui.space_switch_lsw.takeItem(e)
                 continue
 
             item.setText(str(found[0]))
+        return
+
+    def rotate(self, direction):
+        selection = pm.ls(sl=1)
+        rotate_axis = str(self.ui.rotate_axis_cbx.currentText())
+        value = -90 if direction == "minus" else 90
+
+        for sel in selection:
+            params = {
+                rotate_axis: 1,
+                "r": 1,
+                "eu": 1,
+                "fo": 1
+            }
+            pm.rotate(sel, value, **params)
         return
